049fbc9f15bb9c7bf801ae0d1217d0486afe3875
Redkotech <105688748+Redkotech@users.noreply.github.com>
2023-06-26 16:25:47 -0500

    Update mvndsl.groovy

--- mvndsl.groovy
+++ mvndsl.groovy
@@ -12,7 +12,7 @@ mavenJob('mvn-DSLProject') {
     //scm('* * * * *')
      githubPush()
   }
-  goals('clean package')
+  goals('clean package', 'maven-standalone-application/pom.xml')
   
   publishers {
     archiveArtifacts('**/*.war')
