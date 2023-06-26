5dc13b80cda356c74ea535cf1b0de460a42a7284
Redkotech <105688748+Redkotech@users.noreply.github.com>
2023-06-26 15:54:12 -0500

    Create dslproj.groovy

---
+++ dslproj.groovy
@@ -0,0 +1,39 @@
+job('mvn-project') {
+  displayName('maven-DSL-project')
+  description('Builds and deploys a Maven project')
+
+  scm {
+    git('https://github.com/Redkotech/maven-standalone-application.git', 'master')
+  }
+
+  triggers {
+    scm('* * * * *')
+  }
+
+  steps {
+    maven('clean package', 'maven-standalone-application/pom.xml')
+  }
+  publishers {
+    archiveArtifacts('**/*.jar')
+    archiveJunit('**/target/surefire-reports/TEST-*.xml')
+  }
+
+/*  postBuild {
+    always {
+      emailext {
+        recipientProviders {
+          developers()
+        }
+        subject('Build Notification - ${PROJECT_NAME} - Build #${BUILD_NUMBER} - ${BUILD_STATUS}')
+        body('''
+          <p>Build Information:</p>
+          <ul>
+            <li>Project: ${PROJECT_NAME}</li>
+            <li>Build Number: ${BUILD_NUMBER}</li>
+            <li>Status: ${BUILD_STATUS}</li>
+          </ul>
+        ''')
+      }
+    }
+  }*/
+}
